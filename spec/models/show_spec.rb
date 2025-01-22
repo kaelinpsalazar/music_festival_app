8f88ff6c6176fabc5c18a7296ed6a4683dc44baf
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-21 20:13:11 -0700

    had to add cors and fix the config file for tomorrow

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Show, type: :model do
   it { should have_many(:schedules).through(:schedule_shows) }
 
   it { should validate_presence_of(:artist) }
-  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:time) }
 end
